735a71f1640d0ff46f15b47434a9426e21e63aaa
tanvirsiraj <tanvirbinsiraj.edu.bd@gmail.com>
2023-06-10 02:03:35 -0700

    AuthProviders added

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProviders";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
